621144f3979c5492a76038515ed83f59ad5845a5
matias <matias.amado@itscordoba.edu.ar>
2023-11-03 16:37:54 -0500

    Modificacion en funcion extraccion_links_vinos

--- scrap.py
+++ scrap.py
@@ -8,19 +8,10 @@ url = "https://tienda.durigutti.com/"
 def estraccion_links_menu(url):
     response = requests.get(url)
     lista_links = []
-    # Comprobar si la solicitud fue exitosa
     if response.status_code == 200:
-        # Parsear el contenido HTML de la página
         soup = BeautifulSoup(response.text, "html.parser")
-        
-        # Encontrar el menú con la clase "sub-menu"
         menu = soup.find(class_="sub-menu")
-        
-        # Buscar enlaces dentro del menú
         links = menu.find_all("a")
-        
-        
-        # Imprimir los enlaces encontrados
         for link in links:
             direccion = link.get("href")
             print(direccion)
@@ -35,21 +26,22 @@ def estraccion_links_menu(url):
 lista_enlaces_menu = estraccion_links_menu(url)
 
 
-def extraccion_links_vinos(lista_enlaces_menu):                   
+def extraccion_links_vinos(lista_enlaces_menu):
+    vinos_enlaces = []                   
     for enlace in lista_enlaces_menu:
         pagina_vinos = requests.get(enlace)
-
         sopa = BeautifulSoup(pagina_vinos.text, "html.parser")
-
         productos = sopa.find_all(class_="title-wrapper")
-        
         for producto in productos:
             links_vinos = producto.find_all('a')
             print(producto.text.strip()) 
             for link in links_vinos:
+                vinos_enlaces.append(link.get("href"))
                 print(link.get("href"))
                 print("--------------------")
         print("Seccion completada\n-----------------------------")
+    print(vinos_enlaces)
+    return vinos_enlaces
 
 extraccion_links_vinos(lista_enlaces_menu)
     
