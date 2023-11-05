f20d80a1e2e02cc17e5e89c435cea5643eaef811
MarthaCallejo <marthaisabelcallejo@gmail.com>
2023-11-04 21:05:34 -0300

    cambios

--- scrap.py
+++ scrap.py
@@ -14,34 +14,39 @@ def estraccion_links_menu(url):
         links = menu.find_all("a")
         for link in links:
             direccion = link.get("href")
-            print(direccion)
-            lista_links.append(direccion) #A cada enlace que extrae la agrego a la lista
-        print("--------------------------------")
-        print(lista_links)
+            lista_links.append(direccion)  # A cada enlace que extrae lo agrego a la lista
     else:
         print("Error al obtener la página:", response.status_code)
     
     return lista_links
 
-lista_enlaces_menu = estraccion_links_menu(url)
-
-
 def extraccion_links_vinos(lista_enlaces_menu):
-    vinos_enlaces = []                   
+    vinos_enlaces = []
     for enlace in lista_enlaces_menu:
         pagina_vinos = requests.get(enlace)
         sopa = BeautifulSoup(pagina_vinos.text, "html.parser")
         productos = sopa.find_all(class_="title-wrapper")
         for producto in productos:
             links_vinos = producto.find_all('a')
-            print(producto.text.strip()) 
             for link in links_vinos:
                 vinos_enlaces.append(link.get("href"))
-                print(link.get("href"))
-                print("--------------------")
-        print("Seccion completada\n-----------------------------")
-    print(vinos_enlaces)
     return vinos_enlaces
 
-extraccion_links_vinos(lista_enlaces_menu)
-    
+def obtener_descripcion_producto(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        descripcion = soup.find(class_="product-short-description")
+        if descripcion:
+            return descripcion.text.strip()
+        else:
+            return "Descripción no encontrada"
+    else:
+        return "Error al obtener la página:", response.status_code
+
+lista_enlaces_menu = estraccion_links_menu(url)
+vinos_enlaces = extraccion_links_vinos(lista_enlaces_menu)
+
+for vino_url in vinos_enlaces:
+    descripcion = obtener_descripcion_producto(vino_url)
+    print(f"Descripción del producto en {vino_url}:\n{descripcion}\n")
